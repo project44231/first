92d0809fdaa1f036b10c2ef5a81ab2e6fda4a1a0
kumar44231 <Project44231@gmail.com>
2023-12-07 19:59:33 -0500

    First commit

---
+++ first.py
@@ -0,0 +1,25 @@
+import sys
+
+print("Hello World")
+# write a main function that prints hello world
+
+
+def print_full_name():
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    # validate inputs
+    if len(first_name) < 1 or len(last_name) < 1:
+        print("Invalid input")
+        print_full_name()
+
+    full_name = first_name + " " + last_name
+    print(full_name)
+
+def main():
+    print("Hello World")
+    print_full_name()
+
+if __name__ == "__main__":
+    main()
+
+    
